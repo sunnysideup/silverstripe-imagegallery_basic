7bb4ceae5d0c1cdd7aa472bf91a2dcabdfd9c5b5
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 13:41:30 +1300

    PATCH: module clean-up

--- code/ImageGalleryPage.php
+++ code/ImageGalleryPage.php
@@ -1,135 +1,131 @@
 <?php
 
-class ImageGalleryPage extends Page {
+class ImageGalleryPage extends Page
+{
+    private static $icon = "imagegallery_basic/images/treeicons/ImageGalleryPage";
 
-	private static $icon = "imagegallery_basic/images/treeicons/ImageGalleryPage";
+    private static $allowed_children = array("ImageGalleryPage"); //can also be "none";
 
-	private static $allowed_children = array("ImageGalleryPage"); //can also be "none";
+    private static $default_child = "ImageGalleryPage";
 
-	private static $default_child = "ImageGalleryPage";
+    private static $description = "Page used to display images in a gallery";
 
-	private static $description = "Page used to display images in a gallery";
+    private static $has_one = array(
+        "AutomaticallyIncludedFolder" => "Folder"
+    );
 
-	private static $has_one = array(
-		"AutomaticallyIncludedFolder" => "Folder"
-	);
+    private static $has_many = array(
+        "ImageGalleryEntries" => "ImageGalleryEntry"
+    );
 
-	private static $has_many = array(
-		"ImageGalleryEntries" => "ImageGalleryEntry"
-	);
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->addFieldToTab("Root.Gallery", new TreeDropdownField($name = "AutomaticallyIncludedFolderID", $title = "Automatically Included Folder (save page to update) - go to Files and Images section to create folder and upload images.", $sourceObjectName = "Folder"));
+        $gridField = new GridField('images', 'Linked images', $this->ImageGalleryEntries(), GridFieldConfig_RelationEditor::create());
+        $fields->addFieldToTab("Root.Gallery", $gridField);
+        if (class_exists("DataObjectSorterController")) {
+            $fields->addFieldToTab("Root.Gallery", new LiteralField("ImageGalleryEntrySorter", DataObjectSorterController::popup_link("ImageGalleryEntry", $filterField = "ParentID", $filterValue = $this->ID, $linkText = "Sort ".$this->Config()->get("ImageGalleryEntry", "plural_name"), $titleField = "FullTitle")));
+        } else {
+            $fields->addFieldToTab("Root.Gallery", new NumericField($name = "Sort", "Sort index number (the lower the number, the earlier it shows up"));
+        }
+        return $fields;
+    }
 
-	function getCMSFields() {
-		$fields = parent::getCMSFields();
-		$fields->addFieldToTab("Root.Gallery", new TreeDropdownField($name = "AutomaticallyIncludedFolderID", $title = "Automatically Included Folder (save page to update) - go to Files and Images section to create folder and upload images.", $sourceObjectName = "Folder"));
-		$gridField = new GridField('images', 'Linked images', $this->ImageGalleryEntries(), GridFieldConfig_RelationEditor::create());
-		$fields->addFieldToTab("Root.Gallery", $gridField);
-		if(class_exists("DataObjectSorterController")) {
-			$fields->addFieldToTab("Root.Gallery", new LiteralField("ImageGalleryEntrySorter", DataObjectSorterController::popup_link("ImageGalleryEntry", $filterField = "ParentID", $filterValue = $this->ID, $linkText = "Sort ".$this->Config()->get("ImageGalleryEntry", "plural_name"), $titleField = "FullTitle")));
-		}
-		else {
-			$fields->addFieldToTab("Root.Gallery", new NumericField($name = "Sort", "Sort index number (the lower the number, the earlier it shows up"));
-		}
-		return $fields;
-	}
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+        $imageAdded = false;
+        if ($this->AutomaticallyIncludedFolderID) {
+            debug::log("A");
+            if ($folder = Folder::get()->byID($this->AutomaticallyIncludedFolderID)) {
+                if ($files = Image::get()->filter("ParentID", $folder->ID)) {
+                    foreach ($files as $file) {
+                        if (ImageGalleryEntry::get()->filter(array("ImageID" => $file->ID, "ParentID" => $this->ID))->count()) {
+                            //do nothing
+                            //debug::log("already exists");
+                        } else {
+                            $ImageGalleryEntry = new ImageGalleryEntry();
+                            $ImageGalleryEntry->Title = $file->Title;
+                            $ImageGalleryEntry->ImageID = $file->ID;
+                            $ImageGalleryEntry->ParentID = $this->ID;
+                            $ImageGalleryEntry->write();
+                            $imageAdded = true;
+                            //debug::log("writing");
+                        }
+                    }
+                } else {
+                    //debug::log("D");
+                }
+            } else {
+                //debug::log("C");
+            }
+        } else {
+            //debug::log("B");
+        }
+        if ($ImageGalleryEntries = ImageGalleryEntry::get()->filter(array("ParentID" => $this->ID))) {
+            foreach ($ImageGalleryEntries as $ImageGalleryEntry) {
+                $image = Image::get()
+                    ->filter(array("ID" => $ImageGalleryEntry->ImageID))
+                    ->exclude(array("Title" => $ImageGalleryEntry->Title))
+                    ->First();
+                if ($image) {
+                    $image->Title = $image->Name = $ImageGalleryEntry->Title;
+                    $image->write();
+                }
+            }
+        }
+        if ($imageAdded) {
+            //LeftAndMain::force_reload();
+        }
+    }
 
-	public function onBeforeWrite() {
-		parent::onBeforeWrite();
-		$imageAdded = false;
-		if($this->AutomaticallyIncludedFolderID) {
-			debug::log("A");
-			if($folder = Folder::get()->byID($this->AutomaticallyIncludedFolderID)) {
-				if($files = Image::get()->filter("ParentID",$folder->ID)) {
-					foreach($files as $file) {
-						if(ImageGalleryEntry::get()->filter(array("ImageID" => $file->ID, "ParentID" => $this->ID))->count()) {
-							//do nothing
-							//debug::log("already exists");
-						}
-						else {
-							$ImageGalleryEntry = new ImageGalleryEntry();
-							$ImageGalleryEntry->Title = $file->Title;
-							$ImageGalleryEntry->ImageID = $file->ID;
-							$ImageGalleryEntry->ParentID = $this->ID;
-							$ImageGalleryEntry->write();
-							$imageAdded = true;
-							//debug::log("writing");
-						}
-					}
-				}
-				else {
-					//debug::log("D");
-				}
-			}
-			else {
-				//debug::log("C");
-			}
-		}
-		else {
-			//debug::log("B");
-		}
-		if($ImageGalleryEntries = ImageGalleryEntry::get()->filter(array("ParentID" => $this->ID))){
-			foreach($ImageGalleryEntries as $ImageGalleryEntry) {
-				$image = Image::get()
-					->filter(array("ID" => $ImageGalleryEntry->ImageID))
-					->exclude(array("Title" => $ImageGalleryEntry->Title))
-					->First();
-				if($image) {
-					$image->Title = $image->Name = $ImageGalleryEntry->Title;
-					$image->write();
-				}
-			}
-		}
-		if($imageAdded) {
-			//LeftAndMain::force_reload();
-		}
-	}
+    public function NextGallery()
+    {
+        $pages = ImageGalleryPage::get()
+            ->exclude(array("ID" => $this->ID))
+            ->where("TimeDiff(\"Created\",'".$this->Created."') > 0")
+            ->sort("Created", "ASC")
+            ->limit(1);
+        if ($pages) {
+            foreach ($pages as $page) {
+                return $page;
+            }
+        }
+    }
 
-	function NextGallery(){
-		$pages = ImageGalleryPage::get()
-			->exclude(array("ID" => $this->ID))
-			->where("TimeDiff(\"Created\",'".$this->Created."') > 0")
-			->sort("Created", "ASC")
-			->limit(1);
-		if($pages) {
-			foreach($pages as $page) {
-				return $page;
-			}
-		}
-	}
-
-	function PreviousGallery(){
-		$pages = ImageGalleryPage::get()
-			->exclude(array("ID" => $this->ID))
-			->where("TimeDiff(\"Created\",'".$this->Created."') < 0")
-			->sort("Created", "DESC")
-			->limit(1);
-		if($pages) {
-			foreach($pages as $page) {
-				return $page;
-			}
-		}
-	}
-
-
-
+    public function PreviousGallery()
+    {
+        $pages = ImageGalleryPage::get()
+            ->exclude(array("ID" => $this->ID))
+            ->where("TimeDiff(\"Created\",'".$this->Created."') < 0")
+            ->sort("Created", "DESC")
+            ->limit(1);
+        if ($pages) {
+            foreach ($pages as $page) {
+                return $page;
+            }
+        }
+    }
 }
 
-class ImageGalleryPage_Controller extends Page_Controller {
+class ImageGalleryPage_Controller extends Page_Controller
+{
+    private static $allowed_actions = array(
+        "updateimagegalleryentries" => "ADMIN"
+    );
 
-	private static $allowed_actions = array(
-		"updateimagegalleryentries" => "ADMIN"
-	);
+    public function init()
+    {
+        parent::init();
+        if (class_exists("PrettyPhoto")) {
+            PrettyPhoto::include_code();
+        }
+    }
 
-	public function init() {
-		parent::init();
-		if(class_exists("PrettyPhoto")) {
-			PrettyPhoto::include_code();
-		}
-	}
-
-	function updateimagegalleryentries() {
-		$this->onBeforeWrite();
-		return array();
-	}
-
-
+    public function updateimagegalleryentries()
+    {
+        $this->onBeforeWrite();
+        return array();
+    }
 }
--- code/model/ImageGalleryEntry.php
+++ code/model/ImageGalleryEntry.php
@@ -1,45 +1,44 @@
 <?php
 
-class ImageGalleryEntry extends DataObject {
+class ImageGalleryEntry extends DataObject
+{
+    private static $singular_name = "Image Gallery Picture";
 
+    private static $plural_name = "Image Gallery Pictures";
 
-	private static $singular_name = "Image Gallery Picture";
+    private static $db = array(
+        "Title" => "Varchar(100)",
+        "Sort" => "Int"
+    );
 
-	private static $plural_name = "Image Gallery Pictures";
+    private static $has_one = array(
+        "Parent" => "SiteTree",
+        "Image" => "Image"
+    );
 
-	private static $db = array(
-		"Title" => "Varchar(100)",
-		"Sort" => "Int"
-	);
+    private static $searchable_fields = array(
+        "Title" => "PartialMatchFilter"
+    );
 
-	private static $has_one = array(
-		"Parent" => "SiteTree",
-		"Image" => "Image"
-	);
+    private static $summary_fields = array(
+        "Title" => "Title",
+    );
 
-	private static $searchable_fields = array(
-		"Title" => "PartialMatchFilter"
-	);
+    private static $field_labels = array(
+        "Sort" => "Sorting Index Number (lower numbers show first)"
+    );
 
-	private static $summary_fields = array(
-		"Title" => "Title",
-	);
+    //CRUD settings
 
-	private static $field_labels = array(
-		"Sort" => "Sorting Index Number (lower numbers show first)"
-	);
+    private static $default_sort = "Sort ASC, Title ASC";
 
-	//CRUD settings
+    private static $defaults = array(
+        "Sort" => 100
+    );
 
-	private static $default_sort = "Sort ASC, Title ASC";
-
-	private static $defaults = array(
-		"Sort" => 100
-	);
-
-	public function populateDefaults() {
-		parent::populateDefaults();
-		$this->Sort = 100;
-	}
-
+    public function populateDefaults()
+    {
+        parent::populateDefaults();
+        $this->Sort = 100;
+    }
 }
